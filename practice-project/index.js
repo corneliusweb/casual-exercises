1d7a92e5c8c7e1f04e67add823ff1a0e8cec230f
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-10-28 11:11:21 +0100

    Practice for..in object loop

--- practice-project/index.js
+++ practice-project/index.js
@@ -41,3 +41,20 @@ out: for (let i = 0; i < halls.length; i++) {
 	}
 	console.log(result);
 }
+
+let menu = {
+	width: 200,
+	height: 300,
+	title: 'My menu',
+};
+
+function multiplyNumeric(obj) {
+	for (let key in obj) {
+		if (typeof obj[key] === 'number') {
+			obj[key] *= 2;
+		}
+		return obj;
+	}
+}
+
+console.log(multiplyNumeric(menu));
